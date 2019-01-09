827f2ff0d2e0b5ff785e81d8db8ec43e843300de
Tom Schindl <tom.schindl@bestsolution.at>
2019-01-09 11:35:02 +0100

    change license to EPL-1.0

--- org.eclipse.fx.drift/src-native/DriftFX/src/win32/D3D9Texture.cpp
+++ org.eclipse.fx.drift/src-native/DriftFX/src/win32/D3D9Texture.cpp
@@ -1,17 +1,12 @@
 /*
- * Copyright (c) 2018 BestSolution and Others. All rights reserved.
+ * Copyright (c) 2019 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0, which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the
- * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
- * version 2 with the GNU Classpath Exception, which is available at
- * https://www.gnu.org/software/classpath/license.html.
- *
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * Contributors:
+ *     Christoph Caks <ccaks@bestsolution.at> - initial API and implementation
  */
 #include <utils/Logger.h>
 #include "D3D9Texture.h"
